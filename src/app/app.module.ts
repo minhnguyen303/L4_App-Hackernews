e3e6f79a1e074cc908fa9ce466174c4bd2ecbb60
minhnguyen303 <minh09az09@gmail.com>
2021-03-12 07:58:11 +0700

    [Bài tập] Nâng cấp ứng dụng Hackernews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { LikesComponent } from './likes/likes.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LikesComponent
+    LikesComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
